ad7fc1fbe5a194183d5a6abcca99b4af7594d8fe
RickCZG <czg18366118377@yahoo.com>
2017-11-21 21:41:33 +0800

    concurrence

--- base/concurrence.go
+++ base/concurrence.go
@@ -1,9 +1,9 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"runtime"
-// )
+import (
+	"fmt"
+	// "runtime"
+)
 
 // func say(s string) {
 // 	for i := 0; i < 5; i++ {
@@ -16,6 +16,57 @@
 // 	go say("world");
 // 	say("hello");
 // }
+
+// func sum(a []int, c chan int) {
+// 	total := 0;
+// 	for _, v := range a {
+// 		total += v;
+// 	}
+// 	c <- total;
+// }
+
+// func main() {
+// 	a := []int{2, 6, 9, 1, 22, 5};
+
+// 	c := make(chan int);
+// 	go sum(a[:len(a)/2], c);
+// 	go sum(a[len(a)/2:], c);
+// 	go sum(a[:], c);
+// 	x, y ,z := <-c, <-c, <-c;
+
+
+// 	fmt.Println(x, y, z)
+// }
+
+
+
+func main() {
+	c := make(chan int, 2);
+	c <- 1;
+	c <- 2;
+
+	fmt.Println(<-c);
+	fmt.Println(<-c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
